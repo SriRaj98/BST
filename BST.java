998718402de56f7e48d34ebbe17e991847d71049
Sri Raj Malireddi <smalireddi@denalihub.com>
2021-04-05 02:06:32 -0500

    first

--- BST.java
+++ BST.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
+
+
 public class BST{
     
     
     class Node{
         int val;
+        char dup;
         Node left;
         Node right;
         
         Node(int input){
         val = input;
         }}
+        
 
     Node root;
     
@@ -24,6 +30,18 @@ public class BST{
 
     void inor(){
         inorder(root);
+        System.out.println("");
+
+    }
+    void preor(){
+        preorder(root);
+        System.out.println("");
+
+    }
+    void postor(){
+        postorder(root);
+        System.out.println("");
+
     }
     
     Node insert(Node root, int val){
@@ -48,8 +66,85 @@ public class BST{
             return;
         }
         inorder(root.left);
-        System.out.println(root.val);
+        System.out.print(root.val+ ", ");
         inorder(root.right);
+    }
+    
+    void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.val+", ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    
+    void postorder(Node root){
+        if(root == null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val+", ");
+
+    }
+    
+    void bfs(){
+        Queue<Node> trav  = new LinkedList<>();
+        trav.add(root);
+        while(trav.peek()!=null){
+            Node a =trav.poll();
+            System.out.print(a.val+", ");
+            if(a.left != null){
+                trav.add(a.left);
+                
+            }
+            if(a.right != null){
+                trav.add(a.right);
+            }
+            
+        }
+        System.out.println("");
+        return;
+    }
+    void dfs(){
+        Stack<Node> trav  = new Stack<>();
+        trav.push(root);
+        while(!trav.empty()){
+            Node a =trav.pop();
+            System.out.print(a.val+", ");
+            if(a.right != null){
+                trav.push(a.right);
+            }
+            if(a.left != null){
+                trav.push(a.left);
+                
+            }
+
+            
+        }
+        System.out.println("");
+        return;
+    }
+
+    void dfsin(){
+        Stack<Node> trav  = new Stack<>();
+        trav.push(root);
+        Node current = root;
+        while(!trav.empty()){
+            while(current.left != null){
+                trav.push(current.left);
+                current = current.left;
+            }
+            current = trav.pop();
+            System.out.print(current.val+", ");
+            if(current.right != null){
+                current = current.right;
+                trav.push(current);
+            }
+        }
+        System.out.println("");
+        return;
     }
     
     
@@ -63,7 +158,19 @@ public class BST{
         app.in(2);
         app.in(7);
         app.in(4);
+        System.out.println("Inorder");
         app.inor();
+        System.out.println("BFS");
+        app.bfs();
+        System.out.println("DFS");
+        app.dfs();
+        System.out.println("POst");
+        app.postor();
+        System.out.println("Pre");
+        app.preor();
+        System.out.println("DFS Inorder: ");
+        app.dfsin();
+        
     }
 }
 
